d4a55d09bb4e568ae2495f3033d6036417b14a79
tiel0043 <rlamw2000@naver.com>
2023-08-18 15:22:35 +0900

    cors 수정

--- src/main/java/com/likelion/babel/BabelApplication.java
+++ src/main/java/com/likelion/babel/BabelApplication.java
@@ -1,9 +1,12 @@
 package com.likelion.babel;
 
+import com.likelion.babel.config.CorsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CorsConfig.class)
 public class BabelApplication {
 
 	public static void main(String[] args) {
