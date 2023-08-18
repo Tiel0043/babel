01216ab3b5e8fde17995fa7c8263742407bfdee5
tiel0043 <rlamw2000@naver.com>
2023-08-18 17:07:43 +0900

    cors

--- src/main/java/com/likelion/babel/config/CorsConfig.java
+++ src/main/java/com/likelion/babel/config/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // CORS를 적용할 패턴
-                .allowedOrigins("*") // 모든 오리진 허용
+                .allowedOrigins("http://43.202.60.225:5173") // 모든 오리진 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메소드
                 .allowedHeaders("*"); // 모든 헤더 허용
     }
